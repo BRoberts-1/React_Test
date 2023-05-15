899406f74d3b695d0200bb15e9182b4d7d49c48c
Binyamin Roberts <jzroberts@hotmail.com>
2023-05-15 22:40:55 +0300

    added .flatMap()

--- src/sponsors.jsx
+++ src/sponsors.jsx
@@ -9,7 +9,7 @@ const SponsorPage = () => {
     fetch("https://youngstartup.io/api/cwebsite/get_sponsors")
       .then((response) => response.json())
       .then((data) => {
-        const sponsorData = data.map(({ values }) => {
+        const sponsorData = data.flatMap(({ values }) => {
           return values.map(
             ({
               id,
@@ -21,14 +21,7 @@ const SponsorPage = () => {
             }) => {
               const teamMembersArr = JSON.parse(team_members);
               const teamMembers = teamMembersArr.map(
-                ({
-                  id,
-                  firstname,
-                  lastname,
-                  linkedin,
-                  headshot_url,
-                  jobtitle,
-                }) => {
+                ({ firstname, lastname, linkedin, headshot_url, jobtitle }) => {
                   return {
                     firstname,
                     lastname,
@@ -50,90 +43,84 @@ const SponsorPage = () => {
           );
         });
         setSponsors(sponsorData);
-      })
-      .catch((error) => console.error(error));
+      });
   }, []);
 
   return (
     <div className="main-container">
-      {sponsors.map((sponsorCompany) => {
+      {sponsors.flatMap((sponsorCompany) => {
         return (
           <div key={sponsorCompany.id} className="box">
-            <h3 key={sponsorCompany.id} className="partner">
+            <h3 key={sponsorCompany.display_name} className="partner">
               {sponsorCompany.display_name} Partner
             </h3>
             <div className="company-info">
-              {sponsorCompany.map((sponsorCompany) => {
-                return (
-                  <div key={sponsorCompany.id} className="">
-                    <div key={sponsorCompany.id} className="">
-                      <div key={sponsorCompany.id} className="">
-                        <img
-                          key={sponsorCompany.id}
-                          src={sponsorCompany.logo_url}
-                          className="logo"
-                          alt="sponsor logo"
-                        />
-                        <p className="text" key={sponsorCompany.id}>
-                          {sponsorCompany.description}
-                        </p>
-                        <a
-                          href={sponsorCompany.website}
-                          key={sponsorCompany.id}
-                          className="btn"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Learn More
-                        </a>
-                        {sponsorCompany.teamMembers.length > 0 && (
-                          <div className="team-members">
-                            {sponsorCompany.teamMembers.map((member) => {
-                              return (
-                                <div
-                                  key={`${member.firstname}-${member.lastname}`}
-                                  className=""
-                                >
-                                  <div className="">
-                                    <div className="">
-                                      {member.headshot_url && (
-                                        <img
-                                          src={member.headshot_url}
-                                          className=""
-                                          alt={`${member.firstname} ${member.lastname}`}
-                                          width="80"
-                                          height="80"
-                                        />
-                                      )}
-                                      <div className="member-info-container">
-                                        <p className="">
-                                          {`${member.firstname} ${member.lastname}`}
-                                        </p>
-                                        {member.jobtitle && (
-                                          <p className="">{member.jobtitle}</p>
-                                        )}
-                                      </div>
-                                    </div>
-                                    {member.linkedin && (
-                                      <a
-                                        href={member.linkedin}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                      >
-                                        <LinkedInIcon />
-                                      </a>
+              <div key={sponsorCompany.id} className="">
+                <div className="">
+                  <div className="">
+                    <img
+                      key={sponsorCompany.id}
+                      src={sponsorCompany.logo_url}
+                      className="logo"
+                      alt="sponsor logo"
+                    />
+                    <p className="text" key={sponsorCompany.id}>
+                      {sponsorCompany.description}
+                    </p>
+                    <a
+                      href={sponsorCompany.website}
+                      className="btn"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn More
+                    </a>
+                    {sponsorCompany.teamMembers.length > 0 && (
+                      <div className="team-members">
+                        {sponsorCompany.teamMembers.map((member) => {
+                          return (
+                            <div
+                              key={`${member.firstname}-${member.lastname}`}
+                              className=""
+                            >
+                              <div className="">
+                                <div className="">
+                                  {member.headshot_url && (
+                                    <img
+                                      src={member.headshot_url}
+                                      className="member-photo"
+                                      alt={`${member.firstname} ${member.lastname}`}
+                                      width="90"
+                                      height="90"
+                                    />
+                                  )}
+                                  <div className="member-info-container">
+                                    <p className="">
+                                      {`${member.firstname} ${member.lastname}`}
+                                    </p>
+                                    {member.jobtitle && (
+                                      <p className="">{member.jobtitle}</p>
                                     )}
                                   </div>
                                 </div>
-                              );
-                            })}
-                          </div>
-                        )}
+                                {member.linkedin && (
+                                  <a
+                                    href={member.linkedin}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    <LinkedInIcon />
+                                  </a>
+                                )}
+                              </div>
+                            </div>
+                          );
+                        })}
                       </div>
-                    </div>
+                    )}
                   </div>
-                );
-              })}
+                </div>
+              </div>
             </div>
           </div>
         );
